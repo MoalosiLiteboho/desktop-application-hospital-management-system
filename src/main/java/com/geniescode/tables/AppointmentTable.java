6ef01d6bd27a1e818cb85b6d7991c724a37b6fd4
MoalosiLiteboho <litebohojamesmoalosi@gmail.com>
2023-04-01 14:36:23 +0200

    commit changes

---
+++ src/main/java/com/geniescode/tables/AppointmentTable.java
@@ -0,0 +1,13 @@
+package com.geniescode.tables;
+
+import com.geniescode.share.components.panel.Panel;
+
+public class AppointmentTable extends Panel {
+    public AppointmentTable() {
+        initComponents();
+    }
+
+    private void initComponents() {
+
+    }
+}
